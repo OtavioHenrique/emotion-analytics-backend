50285f125984b7c449014964f8f129b0f68366b8
Otavio H. P. Valadares <otaviopvaladares@gmail.com>
2019-10-20 20:01:17 -0300

    create expressions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_20_215652) do
+ActiveRecord::Schema.define(version: 2019_10_20_225822) do
 
   create_table "emotions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "anger"
@@ -21,6 +21,32 @@ ActiveRecord::Schema.define(version: 2019_10_20_215652) do
     t.integer "sadness"
     t.integer "valence"
     t.integer "engagement"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "expressions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "attention"
+    t.integer "brow_furrow"
+    t.integer "brow_raise"
+    t.integer "cheek_raise"
+    t.integer "chin_raiser"
+    t.integer "dimpler"
+    t.integer "eye_closure"
+    t.integer "eye_widen"
+    t.integer "inner_brow_raise"
+    t.integer "jaw_drop"
+    t.integer "lid_tighten"
+    t.integer "lip_corner_depressor"
+    t.integer "lip_press"
+    t.integer "lip_pucker"
+    t.integer "lip_stretch"
+    t.integer "lip_suck"
+    t.integer "mouth_open"
+    t.integer "nose_wrinkle"
+    t.integer "smile"
+    t.integer "smirk"
+    t.integer "upper_lip_raise"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
