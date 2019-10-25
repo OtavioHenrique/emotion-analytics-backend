954a7228321f2a7d24df311a191c0e8a8eee80f6
Otavio H. P. Valadares <otaviopvaladares@gmail.com>
2019-10-24 21:06:07 -0300

    graph endpoint boirplate

---
+++ app/controllers/graph/emotions_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Graph
+  class EmotionsController < ApplicationController
+    def generate
+      render json: { message: "ola" }
+    end
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  post '/test_type', to: 'test_type#create'
-  get '/test_type/:id', to: 'test_type#show'
-  get '/test_type', to: 'test_type#index'
-  patch '/test_type/:id', to: 'test_type#update'
+  post "/test_type", to: "test_type#create"
+  get "/test_type/:id", to: "test_type#show"
+  get "/test_type", to: "test_type#index"
+  patch "/test_type/:id", to: "test_type#update"
 
   resources :tests, only: %i[index show create update]
   resources :emotions, only: %i[index show create update]
   resources :expressions, only: %i[index show create update]
   resources :occurrences, only: %i[index show create update]
+
+  get "/graph/emotions/:id", to: "graph/emotions#generate"
 end
