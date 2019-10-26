d6e798419958fab3f2f59aca14b85a406e4607ed
Otavio H. P. Valadares <otaviopvaladares@gmail.com>
2019-10-26 14:39:53 -0300

    fix typo

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,43 +1,41 @@
 class PeopleController < ApplicationController
-    def create
-      new_people = People.create(
-        name: params['name'],
-        email: params['email'],
-        birth: params['birth'],
-        consent: params['consent'],
-      )
-  
-      render json: new_people, status: 201
-    end 
-  
-    def show
-      people = People.find params['id']
-  
-      render json: people, status: :ok
-    end
-  
-    def index
-      all_people = People.all
-  
-      render json: all_people, status: :ok
-    end
-  
-    def update
-      people = People.find params['id']
-  
-      people.name = params['name'] || people.name
-      people.email = params['email'] || people.email
-      people.birth = params['birth'] || people.birth
-      people.consent = params['consent'] || people.consent
-  
-      people.save
-  
-      render json: people
-    end
+  def create
+    new_people = People.create(
+      name: params['name'],
+      email: params['email'],
+      birth: params['birth']
+    )
 
-    private
-
-    def people_params
-        params.require(:people).permit(:name, :email, :birth, :consent)
-    end
+    render json: new_people, status: 201
   end
+
+  def show
+    people = People.find params['id']
+
+    render json: people, status: :ok
+  end
+
+  def index
+    all_people = People.all
+
+    render json: all_people, status: :ok
+  end
+
+  def update
+    people = People.find params['id']
+
+    people.name = params['name'] || people.name
+    people.email = params['email'] || people.email
+    people.birth = params['birth'] || people.birth
+
+    people.save
+
+    render json: people
+  end
+
+  private
+
+  def people_params
+    params.require(:people).permit(:name, :email, :birth)
+  end
+end
