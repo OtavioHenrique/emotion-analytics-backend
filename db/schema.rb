3096d5feca7ecf4f90efb558fe7e853fc1603255
Otavio H. P. Valadares <otaviopvaladares@gmail.com>
2019-10-29 20:20:06 -0300

    add test_type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_26_185156) do
+ActiveRecord::Schema.define(version: 2019_10_29_231858) do
 
   create_table "emotions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.float "anger"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2019_10_26_185156) do
     t.text "start_url"
     t.text "objective"
     t.boolean "allow_abandonment"
+    t.string "title"
   end
 
   create_table "tests", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
